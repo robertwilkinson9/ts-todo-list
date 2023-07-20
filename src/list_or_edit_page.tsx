86a0188b5cd7debef56422d9d11ae932f69f0ba9
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-07-20 15:17:36 +0100

    final localstoreage changes

--- src/list_or_edit_page.tsx
+++ src/list_or_edit_page.tsx
@@ -3,10 +3,9 @@ import TodoList from './todoList';
 import {TodoData, ListOrEditProps} from './interfaces';
 
 const ListOrEditPage = (props: ListOrEditProps) => {
-  console.log("ListOrEditPage TODOS are ", JSON.stringify(props.todos));
   console.log("ListOrEditPage PROPS are ", JSON.stringify(props));
   if (props.edit_mode) {
-    let todo:TodoData = {due : "", summary: "", text: ""}; 
+    let todo: TodoData = {due: "", summary: "", text: ""}; 
     if (props.edit_due) {
       const found_todo = props.todos.find(todo => {return todo.due === props.edit_due});
       if (found_todo !== undefined) {
@@ -15,9 +14,9 @@ const ListOrEditPage = (props: ListOrEditProps) => {
     }
     console.log("ListOrEditPage TODO is ", JSON.stringify(todo));
     return <Edit todo={todo} todos={props.todos} updater={props.updater} seteditmode={props.seteditmode} />
- } else {
-   return <TodoList seteditmode={props.seteditmode} seteditdue={props.seteditdue} todos={props.todos} setter={props.setter} getTodos={props.getTodos} />
- }
+  } else {
+    return <TodoList seteditmode={props.seteditmode} seteditdue={props.seteditdue} todos={props.todos} setter={props.setter} getTodos={props.getTodos} />
+  }
 }
 
 export default ListOrEditPage;
